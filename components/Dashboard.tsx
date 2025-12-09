51e7562a3d62dbdd1b1663a834f723328d9d6020
Jose Librado Soto Obeso <librado.soto14@hotmail.com>
2025-12-08 23:03:09 -0700

    initial version

---
+++ components/Dashboard.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+import { ScrollView, StyleSheet } from "react-native";
+
+
+const Dashboard = ( { children }:PropsWithChildren) => {
+
+    return(
+        <ScrollView
+            style={{...styles.dashboardContainer}}
+            showsVerticalScrollIndicator={false}
+        >
+            { children}
+        </ScrollView>
+    );
+}
+
+const  styles = StyleSheet.create({
+    dashboardContainer: {
+        flex: 1,
+        width: '90%',
+        padding: 20,
+    }
+});
+
+export default Dashboard;
