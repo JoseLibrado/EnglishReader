8f29729cd752bdcdea5a694debee6119e0662783
José Librado Soto Obeso <43381516+JoseLibrado@users.noreply.github.com>
2025-12-14 22:52:21 -0700

    version beta (#2)

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import CardReading from "@/components/CardReading";
 import ContentCard from "@/components/ContentCard";
 import Dashboard from "@/components/Dashboard";
+import { Mytranscrypts } from "@/data";
 import { useNavigation } from "expo-router";
 import { View } from "react-native";
 
 export default function Index() {
 
   const navigation = useNavigation();
-  
+
   return (
     <View
       style={{
@@ -17,7 +18,7 @@ export default function Index() {
     >
       <Dashboard>
         <CardReading>
-          <ContentCard onPress={ () => navigation.navigate('Sheet' as never)} title="Casual Day"/>
+          <ContentCard onPress={ () => navigation.navigate('Sheet' as never)} title={Mytranscrypts.data[0].name.substring(0,20)} image={Mytranscrypts.data[0].sluck}/>
         </CardReading>
         
       </Dashboard>
