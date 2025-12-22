50833bebbaf24d929a74d1dc24b28bbb4e765cba
Jose Librado Soto Obeso <librado.soto14@hotmail.com>
2025-12-21 17:36:44 -0700

    dinamic items

--- app/index.tsx
+++ app/index.tsx
@@ -2,25 +2,40 @@ import CardReading from "@/components/CardReading";
 import ContentCard from "@/components/ContentCard";
 import Dashboard from "@/components/Dashboard";
 import { Mytranscrypts } from "@/data";
-import { useNavigation } from "expo-router";
+import { useRouter } from "expo-router";
 import { View } from "react-native";
 
 export default function Index() {
 
-  const navigation = useNavigation();
+  const router = useRouter();
 
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: "#414141ff"
+        backgroundColor: '#414141ff',
       }}
     >
       <Dashboard>
-        <CardReading>
-          <ContentCard onPress={ () => navigation.navigate('Sheet' as never)} title={Mytranscrypts.data[0].name.substring(0,20)} image={Mytranscrypts.data[0].sluck}/>
-        </CardReading>
-        
+
+        {
+          Mytranscrypts.data.map(
+            item => <CardReading key={item.id}>
+              <ContentCard
+                onPress={() => router.push({
+                  pathname: '/SheetReader',
+                  params: {
+                    title: item.name.substring(0, 20),
+                    videoLink: item.videoLink,
+                    text: item.transcript
+                  }
+                },
+                )}
+                title={item.name.substring(0, 20)}
+                image={item.sluck} />
+            </CardReading>)
+        }
+
       </Dashboard>
     </View>
   );
